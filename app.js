3219f5ab8d4c409ba13897384b106d948e58ae9e
Alexander Bartolomey <occloxium@gmail.com>
2019-02-24 00:36:09 +0100

    first set of event handlers

---
+++ app.js
@@ -0,0 +1,35 @@
+// import {AJAX} from './js/AJAX';
+import {Entries} from './js/Entries';
+
+(() => {
+  const el_albums = document.querySelector('#albums');
+  const el_singles = document.querySelector('#singles-eps');
+  // const el_more = document.querySelector('#more');
+
+  document.querySelector('nav li').addEventListener('click', (e) => {
+    document.querySelectorAll('nav li.is-active').forEach((el) => {
+      el.classList.remove('is-active');
+    });
+    e.target.classList.add('is-active');
+
+    document.querySelectorAll('main .content-wrapper section').forEach((el) => {
+      el.classList.add('is-hidden');
+    });
+    const target = document.querySelector('nav li.is-active').attributes.getNamedItem('for');
+    document.querySelector('#' + target).classList.remove('is-hidden');
+  });
+
+  const data_albums = Entries.Albums;
+  const data_singles = Entries.Singles;
+  // const data_more = [];
+
+  data_albums.forEach((a) => {
+    el_albums.insertAdjacentHTML('beforeend', a.toString());
+  });
+  data_singles.forEach((a) => {
+    el_singles.insertAdjacentHTML('beforeend', a.toString());
+  });
+
+  document.querySelectorAll('nav li')[0].classList.add('is-active');
+  document.querySelectorAll('main .content-wrapper section')[0].classList.add('is-active');
+})();

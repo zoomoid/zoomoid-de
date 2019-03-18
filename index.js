d2e2322287baff187e087e314d6e8a3b288eecfc
Alexander Bartolomey <occloxium@gmail.com>
2019-03-18 18:14:22 +0100

    removed webserver code
    now only serves staticly build webpage

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 // const cors = require('cors');
-const Entries = require('./api/Entries');
 const app = express();
 const morgan = require('morgan');
 // app.use(cors());
@@ -8,18 +7,6 @@ const morgan = require('morgan');
 app.use(morgan('tiny'));
 app.use(express.static('./client/'));
 
-app.get('/api/albums', (req, res) => {
-  const html = Entries.Albums.reduce((p, c) => {
-    return p + c.toString();
-  }, '');
-  res.send(html);
-});
-app.get('/api/singles', (req, res) => {
-  const html = Entries.Singles.reduce((p, c) => {
-    return p + c.toString();
-  }, '');
-  res.send(html);
-});
 const port = 3000;
 app.listen(port, () => {
   console.log('http server started on port ' + port);

e1bd80ba75fa33924c7579d755dc47dfbec9f468
Alexander Bartolomey <occloxium@gmail.com>
2020-02-01 14:32:46 +0100

    Remove obsolete components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,19 +1,15 @@
-import Banner from './Banner';
 import Block from './Block';
 import Card from './Card';
 import CookieBanner from './CookieBanner';
 import Footer from './Footer';
-import Navigation from './Navigation';
 import SlideAnchor from './SlideAnchor';
 import WorkContainer from './WorkContainer';
 
 export {
-  Banner,
   Block,
   Card,
   CookieBanner,
   Footer,
-  Navigation,
   SlideAnchor,
   WorkContainer,
 };

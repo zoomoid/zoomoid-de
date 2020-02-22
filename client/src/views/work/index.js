f0c77c2c4bc13360b7dd498d90e0bf5b2f97cb5d
Alexander Bartolomey <occloxium@gmail.com>
2020-02-22 21:44:03 +0100

    Remove old unused components
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- client/src/views/work/index.js
+++ client/src/views/work/index.js
@@ -1,18 +1,12 @@
 import LIAMM from './LIAMM';
-import Datkom from './Datkom';
 import EigenräumeExtendedEdition from './EigenräumeExtendedEdition';
 import Eigenräume from './Eigenräume';
-import Interstellar from './Interstellar';
-import AtomicSpark from './AtomicSpark';
 import PermanentlyMoving from './PermanentlyMoving';
 import PublicTransportation from './PublicTransportation';
 export {
   LIAMM,
-  Datkom,
   EigenräumeExtendedEdition,
   Eigenräume,
-  AtomicSpark,
-  Interstellar,
   PermanentlyMoving,
   PublicTransportation
 };

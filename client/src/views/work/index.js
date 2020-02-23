18933d3cc232056a37458b9c6650a0ad3c6f5a24
Alexander Bartolomey <occloxium@gmail.com>
2020-02-23 18:25:17 +0100

    Migrate to dart-sass
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- client/src/views/work/index.js
+++ client/src/views/work/index.js
@@ -3,10 +3,13 @@ import EigenräumeExtendedEdition from './EigenräumeExtendedEdition';
 import Eigenräume from './Eigenräume';
 import PermanentlyMoving from './PermanentlyMoving';
 import PublicTransportation from './PublicTransportation';
+import SchwerelosRemix from './SchwerelosRemix';
+
 export {
   LIAMM,
   EigenräumeExtendedEdition,
   Eigenräume,
   PermanentlyMoving,
-  PublicTransportation
+  PublicTransportation,
+  SchwerelosRemix
 };

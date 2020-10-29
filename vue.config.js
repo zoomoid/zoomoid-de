fad21b783be0f28699e0669bb73d7032c19f1ebc
Alexander Bartolomey <occloxium@gmail.com>
2020-10-29 11:49:42 +0100

    build: :ambulance: fix vue loader webpack config
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -12,43 +12,42 @@ module.exports = {
   configureWebpack: config => {
     // eslint-disable-next-line no-undef
     if(process.env.NODE_ENV === "production") {
-      config.module = {
-        rules: [
-          {
-            test: /\.(jpe?g|png|gif|svg)$/i,
-            use: [
-              {
-                loader: "url-loader", // Or `url-loader` or your other loader
-              },
-              {
-                loader: ImageMinimizerPlugin.loader,
-                options: {
-                  severityError: "warning",
-                  minimizerOptions: {
-                    // Lossless optimization with custom option
-                    // Feel free to experiment with options for better result for you
-                    plugins: [
-                      ["gifsicle", { interlaced: true }],
-                      ["mozjpeg", { progressive: true }],
-                      ["pngquant", { optimizationLevel: 5 }],
-                      [
-                        "svgo",
-                        {
-                          plugins: [
-                            {
-                              removeViewBox: false,
-                            },
-                          ],
-                        },
-                      ],
+      config.module.rules = [
+        ...config.module.rules,
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          use: [
+            {
+              loader: "url-loader", // Or `url-loader` or your other loader
+            },
+            {
+              loader: ImageMinimizerPlugin.loader,
+              options: {
+                severityError: "warning",
+                minimizerOptions: {
+                  // Lossless optimization with custom option
+                  // Feel free to experiment with options for better result for you
+                  plugins: [
+                    ["gifsicle", { interlaced: true }],
+                    ["mozjpeg", { progressive: true }],
+                    ["pngquant", { optimizationLevel: 5 }],
+                    [
+                      "svgo",
+                      {
+                        plugins: [
+                          {
+                            removeViewBox: false,
+                          },
+                        ],
+                      },
                     ],
-                  },
+                  ],
                 },
               },
-            ],
-          },
-        ],
-      }
+            }
+          ],
+        },
+      ]
     }
   },
 };

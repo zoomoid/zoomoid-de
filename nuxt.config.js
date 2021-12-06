20536f96e898a0cb021e9a37dc21324e4233659f
Alexander Bartolomey <occloxium@gmail.com>
2021-12-06 09:03:37 +0100

    fix: page title from package.json
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'zoomoid-de',
+    title: 'zoomoid.de',
     htmlAttrs: {
       lang: 'en'
     },

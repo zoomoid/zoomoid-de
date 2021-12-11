124e665b9726408ddeaf66629780b6b31d2c0893
Alexander Bartolomey <occloxium@gmail.com>
2021-12-11 23:57:34 +0100

    chore!: upgrade to tailwindcss@3.0.0
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,13 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
-  purge: [
+  content: [
     "./components/**/*.{vue,js}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
   ],
-  darkMode: false, // or 'media' or 'class'
-  mode: "jit",
   theme: {
     extend: {
-      colors: {
-        ...colors,
-      },
       fontFamily: {
         sans: ['"basiersquare"'],
         serif: ['"novela"'],

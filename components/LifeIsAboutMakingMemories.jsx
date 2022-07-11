1277bf0bc1ad289ff5f5554367b2dd05a056cc1d
Alexander Bartolomey <occloxium@gmail.com>
2022-07-11 13:57:38 +0200

    fix: some linter complaints
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- components/LifeIsAboutMakingMemories.jsx
+++ components/LifeIsAboutMakingMemories.jsx
@@ -5,16 +5,16 @@ import liamm_banner_vectorized from "public/img/assets/liamm-banner-vectorized.s
 export default function LifeIsAboutMakingMemories() {
   return (
     <section id="life-is-about-making-memories" className="bg-neutral-200 text-neutral-800 py-32 relative">
-      <div class="absolute right-0 transform -translate-x-20 -rotate-45 scale-[3] opacity-30">
+      <div className="absolute right-0 transform -translate-x-20 -rotate-45 scale-[3] opacity-30">
         <Image src={liamm_banner_vectorized}></Image>
       </div>
-      <div class="container max-w-screen-2xl md:grid grid-cols-2 gap-8">
+      <div className="container max-w-screen-2xl md:grid grid-cols-2 gap-8">
         <div>
           <h2 className="font-sans font-semibold uppercase text-6xl my-4">
             “Life Is About Making Memories”
           </h2>
           <p className="text-2xl leading-normal">
-            “Life Is About Making Memories” is Zoomoid's first proper album
+            “Life Is About Making Memories” is Zoomoid&apos;s first proper album
             release. It features 11 tracks, a proper intro and outro, and tries
             to hit those nostalgia notes, with tracks thematically and sonically
             covering video games, movies, tv series, and moments from childhood

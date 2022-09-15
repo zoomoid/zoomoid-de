6b98e825f5770b67d4ae2270d66f2e23d292130a
Alexander Bartolomey <occloxium@gmail.com>
2022-09-15 14:01:02 +0200

    chore: remove obsolete api endpoint for static content
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- pages/api/releases/[rid].js
+++
@@ -1,7 +0,0 @@
-import { releases } from ".";
-
-export default function handler(req, res) {
-  const { rid } = req.query
-  const release = releases.find((r) => r.anchor === rid)
-  res.status(200).json({ ...release });
-}
--- pages/api/releases/covers.js
+++
@@ -1,6 +0,0 @@
-import { releases } from ".";
-
-export default function handler(req, res) {
-  const covers = releases.map((r) => r.cover.url)
-  res.status(200).json([...covers]);
-}

2e06abb3e00dea4c2689c05f2d58d0bb36e91071
Alexander Bartolomey <occloxium@gmail.com>
2022-09-15 14:31:27 +0200

    fix: ordering issue of icon font-face causing font-size to be misordered
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,3 @@
-import "../styles/fonts/basier/stylesheet.css";
-import "../styles/fonts/material-icons/stylesheet.css";
-import "../styles/fonts/novela/stylesheet.css";
 import "../styles/globals.css";
 import React from "react";
 

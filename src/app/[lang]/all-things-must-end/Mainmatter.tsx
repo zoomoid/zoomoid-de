a27e19f4d8d3aa8e58ea6cd3415e1ffcd70db23e
Alexander Bartolomey <occloxium@gmail.com>
2023-12-27 17:15:39 +0100

    feat: translate biography
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- src/app/[lang]/all-things-must-end/Mainmatter.tsx
+++ src/app/[lang]/all-things-must-end/Mainmatter.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import { useRef } from "react"
+import { useContext, useRef } from "react";
 import Biography from "./Biography";
 import Album from "./Album";
+import { LocaleContext } from "@/context/locale.context";
 
 export default function Mainmatter() {
-  const albumSkipRef = useRef<HTMLDivElement>(null)
+  const albumSkipRef = useRef<HTMLDivElement>(null);
+  const { state } = useContext(LocaleContext);
 
   return (
     <>
-      <Biography skipRef={albumSkipRef}></Biography>
+      {state.lang === "de" ? (
+        <Biography.DE skipRef={albumSkipRef}></Biography.DE>
+      ) : (
+        <Biography.EN skipRef={albumSkipRef}></Biography.EN>
+      )}
       <Album skipRef={albumSkipRef}></Album>
     </>
-  )
+  );
 }

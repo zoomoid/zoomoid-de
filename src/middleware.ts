7a7eb24bc80efb14bc843a8b595de590a2a76893
Alexander Bartolomey <occloxium@gmail.com>
2023-12-29 01:56:20 +0100

    fix: middleware path checks
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -32,7 +32,7 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // Skip all internal paths (_next)
-    '/((?!api|_next/image|_next/static|img|fonts|_ipx|static).*)',
+    '/((?!api|_next|img|fonts|_ipx|static).*)',
     // Optional: only run on root (/) URL
     // '/'
   ],
